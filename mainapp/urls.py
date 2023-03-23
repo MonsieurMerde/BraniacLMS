82a9e3677e6d8da978875e7aedf8b4ffef92ed95
Joe <totenlciht@mail.ru>
2023-03-23 12:44:45 +0500

    lesson_05: New config and migration

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,14 +1,17 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import include, path
-from django.views.generic import RedirectView
+from django.urls import path
+
+from mainapp import views
+from mainapp.apps import MainappConfig
+
+app_name = MainappConfig.name
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", RedirectView.as_view(url="mainapp/")),
-    path("mainapp/", include("mainapp.urls", namespace="mainapp")),
+    path("", views.MainPageView.as_view(), name="main_page"),
+    path("news/", views.NewsPageView.as_view(), name="news"),
+    path("news/<int:pk>/", views.NewsPageDetailView.as_view(), name="news_detail"),
+    path("courses/", views.CoursesListView.as_view(), name="courses"),
+    path("courses/<int:pk>/", views.CoursesDetailView.as_view(), name="courses_detail"),
+    path("contacts/", views.ContactsPageView.as_view(), name="contacts"),
+    path("doc_site/", views.DocSitePageView.as_view(), name="doc_site"),
+    path("login/", views.LoginPageView.as_view(), name="login"),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
