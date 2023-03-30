19c694a21d3d6a0af8272850c168681009bd619a
Joe <totenlciht@mail.ru>
2023-03-30 16:48:54 +0500

    lesson_05: Email auth

--- config/settings.py
+++ config/settings.py
@@ -114,6 +114,7 @@ AUTH_USER_MODEL = "authapp.CustomUser"
 AUTHENTICATION_BACKENDS = (
     "social_core.backends.github.GithubOAuth2",
     "social_core.backends.vk.VKOAuth2",
+    "authapp.auth.EmailAuthBackend",
     "django.contrib.auth.backends.ModelBackend",
 )
 
